b8af60e867395fc8be752c0004b83d198ca75fb0
sankarshan <ssankarshan636@gmail.com>
2025-08-02 02:09:06 +0530

    Update CORS origin to allow requests from production URL

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://well-ness-app.vercel.app',
     credentials: true
 }))
 
